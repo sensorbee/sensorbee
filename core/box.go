062422527dfdbfc5a11e4241e9b53ba0a187824a
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-04-02 17:58:30 +0900

    Add files for a small hackathon

--- core/box.go
+++ core/box.go
@@ -1,4 +1,11 @@
 package core
 
-type Box struct {
+type Box interface {
+	Process(t *Tuple, s Sink) error
+}
+
+type BoxFunc func(t *Tuple, s Sink) error
+
+func (b BoxFunc) Process(t *Tuple, s Sink) error {
+	return b(t, s)
 }
--- core/tuple.go
+++
@@ -1,4 +0,0 @@
-package core
-
-type Tuple interface {
-}
