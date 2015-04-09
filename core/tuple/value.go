078eeb3d02df80c449674d9d2a31531d1faa6670
TANAKA Daisuke <tanakad@preferred.jp>
2015-04-09 18:18:35 +0900

    add error function to value methods

--- core/tuple/value.go
+++ core/tuple/value.go
@@ -1,6 +1,7 @@
 package tuple
 
 import (
+	"errors"
 	"fmt"
 	"github.com/mattn/go-scan"
 	"time"
@@ -8,17 +9,20 @@ import (
 
 type Value interface {
 	Type() TypeID
-	Int() Int
-	Float() Float
-	String() String
-	Blob() Blob
-	Timestamp() Timestamp
-	Array() Array
-	Map() Map
+	Int() (Int, error)
+	Float() (Float, error)
+	String() (String, error)
+	Blob() (Blob, error)
+	Timestamp() (Timestamp, error)
+	Array() (Array, error)
+	Map() (Map, error)
 }
 
 // TODO: Provide NewMap(map[string]interface{}) Map
-// TODO: Need Implemet Test Code
+
+func Error(from TypeID, to TypeID) error {
+	return errors.New(fmt.Sprintf("unsupported cast %v from %v", to.String(), from.String()))
+}
 
 type TypeID int
 
@@ -60,33 +64,32 @@ func (i Int) Type() TypeID {
 	return TypeInt
 }
 
-func (i Int) Int() Int {
-	return i
+func (i Int) Int() (Int, error) {
+	return i, nil
 }
 
-func (i Int) Float() Float {
-	return Float(i)
+func (i Int) Float() (Float, error) {
+	return Float(i), nil
 }
 
-func (i Int) String() String {
-	return String(fmt.Sprint(i))
+func (i Int) String() (String, error) {
+	return String(fmt.Sprint(i)), nil
 }
 
-func (i Int) Blob() Blob {
-	// TODO: This method should return an error instead of panic
-	panic("unsupported conversion")
+func (i Int) Blob() (Blob, error) {
+	return nil, Error(i.Type(), TypeBlob)
 }
 
-func (i Int) Timestamp() Timestamp {
-	panic("unsupported conversion")
+func (i Int) Timestamp() (Timestamp, error) {
+	return Timestamp{}, Error(i.Type(), TypeTimestamp)
 }
 
-func (i Int) Array() Array {
-	panic("unsupported conversion")
+func (i Int) Array() (Array, error) {
+	return nil, Error(i.Type(), TypeArray)
 }
 
-func (i Int) Map() Map {
-	panic("unsupported conversion")
+func (i Int) Map() (Map, error) {
+	return nil, Error(i.Type(), TypeMap)
 }
 
 type Float float64
@@ -95,32 +98,32 @@ func (f Float) Type() TypeID {
 	return TypeFloat
 }
 
-func (f Float) Int() Int {
-	return Int(f)
+func (f Float) Int() (Int, error) {
+	return Int(f), nil
 }
 
-func (f Float) Float() Float {
-	return f
+func (f Float) Float() (Float, error) {
+	return f, nil
 }
 
-func (f Float) String() String {
-	return String(fmt.Sprint(f))
+func (f Float) String() (String, error) {
+	return String(fmt.Sprint(f)), nil
 }
 
-func (f Float) Blob() Blob {
-	panic("unsupported conversion")
+func (f Float) Blob() (Blob, error) {
+	return nil, Error(f.Type(), TypeFloat)
 }
 
-func (f Float) Timestamp() Timestamp {
-	panic("unsupported conversion")
+func (f Float) Timestamp() (Timestamp, error) {
+	return Timestamp{}, Error(f.Type(), TypeTimestamp)
 }
 
-func (f Float) Array() Array {
-	panic("unsupported conversion")
+func (f Float) Array() (Array, error) {
+	return nil, Error(f.Type(), TypeArray)
 }
 
-func (f Float) Map() Map {
-	panic("unsupported conversion")
+func (f Float) Map() (Map, error) {
+	return nil, Error(f.Type(), TypeMap)
 }
 
 type String string
@@ -129,32 +132,32 @@ func (s String) Type() TypeID {
 	return TypeString
 }
 
-func (s String) Int() Int {
-	panic("unsupported conversion")
+func (s String) Int() (Int, error) {
+	return 0, Error(s.Type(), TypeInt)
 }
 
-func (s String) Float() Float {
-	panic("unsupported conversion")
+func (s String) Float() (Float, error) {
+	return 0, Error(s.Type(), TypeFloat)
 }
 
-func (s String) String() String {
-	return s
+func (s String) String() (String, error) {
+	return s, nil
 }
 
-func (s String) Blob() Blob {
-	panic("unsupported conversion")
+func (s String) Blob() (Blob, error) {
+	return nil, Error(s.Type(), TypeBlob)
 }
 
-func (s String) Timestamp() Timestamp {
-	panic("unsupported conversion")
+func (s String) Timestamp() (Timestamp, error) {
+	return Timestamp{}, Error(s.Type(), TypeTimestamp)
 }
 
-func (s String) Array() Array {
-	panic("unsupported conversion")
+func (s String) Array() (Array, error) {
+	return nil, Error(s.Type(), TypeArray)
 }
 
-func (s String) Map() Map {
-	panic("unsupported conversion")
+func (s String) Map() (Map, error) {
+	return nil, Error(s.Type(), TypeMap)
 }
 
 type Blob []byte
@@ -163,32 +166,32 @@ func (b Blob) Type() TypeID {
 	return TypeBlob
 }
 
-func (b Blob) Int() Int {
-	panic("unsupported conversion")
+func (b Blob) Int() (Int, error) {
+	return 0, Error(b.Type(), TypeInt)
 }
 
-func (b Blob) Float() Float {
-	panic("unsupported conversion")
+func (b Blob) Float() (Float, error) {
+	return 0, Error(b.Type(), TypeFloat)
 }
 
-func (b Blob) String() String {
-	panic("unsupported conversion")
+func (b Blob) String() (String, error) {
+	return "", Error(b.Type(), TypeString)
 }
 
-func (b Blob) Blob() Blob {
-	return b
+func (b Blob) Blob() (Blob, error) {
+	return b, nil
 }
 
-func (b Blob) Timestamp() Timestamp {
-	panic("unsupported conversion")
+func (b Blob) Timestamp() (Timestamp, error) {
+	return Timestamp{}, Error(b.Type(), TypeTimestamp)
 }
 
-func (b Blob) Array() Array {
-	panic("unsupported conversion")
+func (b Blob) Array() (Array, error) {
+	return nil, Error(b.Type(), TypeArray)
 }
 
-func (b Blob) Map() Map {
-	panic("unsupported conversion")
+func (b Blob) Map() (Map, error) {
+	return nil, Error(b.Type(), TypeMap)
 }
 
 type Timestamp time.Time
@@ -197,32 +200,32 @@ func (t Timestamp) Type() TypeID {
 	return TypeTimestamp
 }
 
-func (t Timestamp) Int() Int {
-	panic("unsupported conversion")
+func (t Timestamp) Int() (Int, error) {
+	return 0, Error(t.Type(), TypeInt)
 }
 
-func (t Timestamp) Float() Float {
-	panic("unsupported conversion")
+func (t Timestamp) Float() (Float, error) {
+	return 0, Error(t.Type(), TypeFloat)
 }
 
-func (t Timestamp) String() String {
-	panic("unsupported conversion")
+func (t Timestamp) String() (String, error) {
+	return "", Error(t.Type(), TypeString)
 }
 
-func (t Timestamp) Blob() Blob {
-	panic("unsupported conversion")
+func (t Timestamp) Blob() (Blob, error) {
+	return nil, Error(t.Type(), TypeBlob)
 }
 
-func (t Timestamp) Timestamp() Timestamp {
-	return t
+func (t Timestamp) Timestamp() (Timestamp, error) {
+	return t, nil
 }
 
-func (t Timestamp) Array() Array {
-	panic("unsupported conversion")
+func (t Timestamp) Array() (Array, error) {
+	return nil, Error(t.Type(), TypeArray)
 }
 
-func (t Timestamp) Map() Map {
-	panic("unsupported conversion")
+func (t Timestamp) Map() (Map, error) {
+	return nil, Error(t.Type(), TypeMap)
 }
 
 type Array []Value
@@ -231,32 +234,32 @@ func (a Array) Type() TypeID {
 	return TypeArray
 }
 
-func (a Array) Int() Int {
-	panic("unsupported conversion")
+func (a Array) Int() (Int, error) {
+	return 0, Error(a.Type(), TypeInt)
 }
 
-func (a Array) Float() Float {
-	panic("unsupported conversion")
+func (a Array) Float() (Float, error) {
+	return 0, Error(a.Type(), TypeFloat)
 }
 
-func (a Array) String() String {
-	panic("unsupported conversion")
+func (a Array) String() (String, error) {
+	return "", Error(a.Type(), TypeString)
 }
 
-func (a Array) Blob() Blob {
-	panic("unsupported conversion")
+func (a Array) Blob() (Blob, error) {
+	return nil, Error(a.Type(), TypeBlob)
 }
 
-func (a Array) Timestamp() Timestamp {
-	panic("unsupported conversion")
+func (a Array) Timestamp() (Timestamp, error) {
+	return Timestamp{}, Error(a.Type(), TypeTimestamp)
 }
 
-func (a Array) Array() Array {
-	return a
+func (a Array) Array() (Array, error) {
+	return a, nil
 }
 
-func (a Array) Map() Map {
-	panic("unsupported conversion")
+func (a Array) Map() (Map, error) {
+	return nil, Error(a.Type(), TypeMap)
 }
 
 func (a Array) toArrayInterface() []interface{} {
@@ -265,9 +268,11 @@ func (a Array) toArrayInterface() []interface{} {
 		var e interface{}
 		switch v.Type() {
 		case TypeArray:
-			e = v.Array().toArrayInterface()
+			a, _ := v.Array()
+			e = a.toArrayInterface()
 		case TypeMap:
-			e = v.Map().toMapInterface()
+			m, _ := v.Map()
+			e = m.toMapInterface()
 		default:
 			e = v
 		}
@@ -282,32 +287,32 @@ func (m Map) Type() TypeID {
 	return TypeMap
 }
 
-func (m Map) Int() Int {
-	panic("unsupported conversion")
+func (m Map) Int() (Int, error) {
+	return 0, Error(m.Type(), TypeInt)
 }
 
-func (m Map) Float() Float {
-	panic("unsupported conversion")
+func (m Map) Float() (Float, error) {
+	return 0, Error(m.Type(), TypeFloat)
 }
 
-func (m Map) String() String {
-	panic("unsupported conversion")
+func (m Map) String() (String, error) {
+	return "", Error(m.Type(), TypeString)
 }
 
-func (m Map) Blob() Blob {
-	panic("unsupported conversion")
+func (m Map) Blob() (Blob, error) {
+	return nil, Error(m.Type(), TypeBlob)
 }
 
-func (m Map) Timestamp() Timestamp {
-	panic("unsupported conversion")
+func (m Map) Timestamp() (Timestamp, error) {
+	return Timestamp{}, Error(m.Type(), TypeTimestamp)
 }
 
-func (m Map) Array() Array {
-	panic("unsupported conversion")
+func (m Map) Array() (Array, error) {
+	return nil, Error(m.Type(), TypeArray)
 }
 
-func (m Map) Map() Map {
-	return m
+func (m Map) Map() (Map, error) {
+	return m, nil
 }
 
 func (m Map) Get(path string) (Value, error) {
@@ -324,9 +329,11 @@ func (m Map) toMapInterface() map[string]interface{} {
 	for k, v := range m {
 		switch v.Type() {
 		case TypeArray:
-			t[k] = v.Array().toArrayInterface()
+			a, _ := v.Array()
+			t[k] = a.toArrayInterface()
 		case TypeMap:
-			t[k] = v.Map().toMapInterface()
+			m, _ := v.Map()
+			t[k] = m.toMapInterface()
 		default:
 			t[k] = v
 		}
