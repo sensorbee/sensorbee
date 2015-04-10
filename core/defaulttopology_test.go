de22a37c9e7c9886ded6f5d1c2d338e2f54fecb0
Tobias Pfeiffer <tgp@preferred.jp>
2015-04-10 16:39:43 +0900

    add AddSink to DefaultTopologyBuilder

--- core/defaulttopology_test.go
+++ core/defaulttopology_test.go
@@ -15,6 +15,7 @@ func TestDefaultTopology(t *testing.T) {
 		tb := NewDefaultTopologyBuilder()
 		s := &DefaultSource{}
 		b := &DefaultBox{}
+		si := &DefaultSink{}
 		var err DeclarerError
 
 		Convey("when using a source name twice", func() {
@@ -30,11 +31,23 @@ func TestDefaultTopology(t *testing.T) {
 		})
 
 		Convey("when using a source name with a box name", func() {
-			err = tb.AddBox("myBoxSource", b)
+			err = tb.AddBox("someName", b)
 			So(err, ShouldNotBeNil)
 			So(err.Err(), ShouldBeNil)
 
-			err = tb.AddSource("myBoxSource", s)
+			err = tb.AddSource("someName", s)
+			So(err, ShouldNotBeNil)
+			Convey("adding should fail", func() {
+				So(err.Err(), ShouldNotBeNil)
+			})
+		})
+
+		Convey("when using a source name with a sink name", func() {
+			err = tb.AddSink("someName", si)
+			So(err, ShouldNotBeNil)
+			So(err.Err(), ShouldBeNil)
+
+			err = tb.AddSource("someName", s)
 			So(err, ShouldNotBeNil)
 			Convey("adding should fail", func() {
 				So(err.Err(), ShouldNotBeNil)
@@ -49,6 +62,66 @@ func TestDefaultTopology(t *testing.T) {
 			err = tb.AddBox("myBox", b)
 			So(err, ShouldNotBeNil)
 			Convey("the second time should fail", func() {
+				So(err.Err(), ShouldNotBeNil)
+			})
+		})
+
+		Convey("when using a box name with a source name", func() {
+			err = tb.AddSource("someName", s)
+			So(err, ShouldNotBeNil)
+			So(err.Err(), ShouldBeNil)
+
+			err = tb.AddBox("someName", b)
+			So(err, ShouldNotBeNil)
+			Convey("adding should fail", func() {
+				So(err.Err(), ShouldNotBeNil)
+			})
+		})
+
+		Convey("when using a box name with a sink name", func() {
+			err = tb.AddSink("someName", si)
+			So(err, ShouldNotBeNil)
+			So(err.Err(), ShouldBeNil)
+
+			err = tb.AddBox("someName", b)
+			So(err, ShouldNotBeNil)
+			Convey("adding should fail", func() {
+				So(err.Err(), ShouldNotBeNil)
+			})
+		})
+
+		Convey("when using a sink name twice", func() {
+			err = tb.AddSink("mySink", si)
+			So(err, ShouldNotBeNil)
+			So(err.Err(), ShouldBeNil)
+
+			err = tb.AddSink("mySink", si)
+			So(err, ShouldNotBeNil)
+			Convey("the second time should fail", func() {
+				So(err.Err(), ShouldNotBeNil)
+			})
+		})
+
+		Convey("when using a sink name with a source name", func() {
+			err = tb.AddSource("someName", s)
+			So(err, ShouldNotBeNil)
+			So(err.Err(), ShouldBeNil)
+
+			err = tb.AddSink("someName", si)
+			So(err, ShouldNotBeNil)
+			Convey("adding should fail", func() {
+				So(err.Err(), ShouldNotBeNil)
+			})
+		})
+
+		Convey("when using a sink name with a box name", func() {
+			err = tb.AddBox("someName", b)
+			So(err, ShouldNotBeNil)
+			So(err.Err(), ShouldBeNil)
+
+			err = tb.AddSink("someName", si)
+			So(err, ShouldNotBeNil)
+			Convey("adding should fail", func() {
 				So(err.Err(), ShouldNotBeNil)
 			})
 		})
