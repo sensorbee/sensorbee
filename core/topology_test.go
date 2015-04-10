dc9a1283ed6c1e293e8452e10d7b18a15c02c4d6
Tobias Pfeiffer <tgp@preferred.jp>
2015-04-10 16:02:35 +0900

    add error handling to fluent interface

--- core/topology_test.go
+++ core/topology_test.go
@@ -13,7 +13,7 @@ func (this *DummyTopology) Run() {}
 type DummyTopologyBuilder struct{}
 
 func (this *DummyTopologyBuilder) AddSource(name string, source Source) SourceDeclarer {
-	return nil
+	return &DummySourceDeclarer{}
 }
 func (this *DummyTopologyBuilder) AddBox(name string, box Box) BoxDeclarer {
 	return &DummyBoxDeclarer{}
@@ -25,9 +25,29 @@ func (this *DummyTopologyBuilder) Build() Topology {
 	return &DummyTopology{}
 }
 
+type DummySourceDeclarer struct{}
+
+func (this *DummySourceDeclarer) Err() error {
+	return nil
+}
+
 type DummyBoxDeclarer struct{}
 
 func (this *DummyBoxDeclarer) Input(name string, schema *Schema) BoxDeclarer {
+	return &DummyBoxDeclarer{}
+}
+
+func (this *DummyBoxDeclarer) Err() error {
+	return nil
+}
+
+type DummySinkDeclarer struct{}
+
+func (this *DummySinkDeclarer) Input(name string) SinkDeclarer {
+	return &DummySinkDeclarer{}
+}
+
+func (this *DummySinkDeclarer) Err() error {
 	return nil
 }
 
@@ -53,6 +73,12 @@ func (this *DummyBox) OutputSchema(s []*Schema) (*Schema, error) {
 	return nil, nil
 }
 
+type DummySink struct{}
+
+func (this *DummySink) Write(t *tuple.Tuple) error {
+	return nil
+}
+
 func TestTopology(t *testing.T) {
 	Convey("Given dummy topology builder, to build topology", t, func() {
 
@@ -70,5 +96,44 @@ func TestTopology(t *testing.T) {
 		Convey("It should satisfy Topology interface", func() {
 			So(t, ShouldNotBeNil)
 		})
+	})
+
+	Convey("Given dummy topology builder, to build complex topology", t, func() {
+
+		so1 := &DummySource{}
+		so2 := &DummySource{}
+		b1 := &DummyBox{}
+		b2 := &DummyBox{}
+		b3 := &DummyBox{}
+		si1 := &DummySink{}
+		si2 := &DummySink{}
+
+		tb := DummyTopologyBuilder{}
+
+		var err DeclarerError
+		err = tb.AddSource("so1", so1)
+		So(err.Err(), ShouldBeNil)
+		err = tb.AddSource("so2", so2)
+		So(err.Err(), ShouldBeNil)
+		err = tb.AddBox("b1", b1).
+			Input("so1", nil).
+			Input("so2", nil)
+		So(err.Err(), ShouldBeNil)
+		err = tb.AddBox("b2", b2).
+			Input("so1", nil)
+		So(err.Err(), ShouldBeNil)
+		err = tb.AddBox("b3", b3).
+			Input("b1", nil)
+		So(err.Err(), ShouldBeNil)
+		err = tb.AddSink("si1", si1).
+			Input("b2").
+			Input("b1").
+			Input("so1")
+		So(err.Err(), ShouldBeNil)
+		err = tb.AddSink("si2", si2).
+			Input("b2").
+			Input("b3")
+		So(err.Err(), ShouldBeNil)
+
 	})
 }
