b5c29f994b2a77d97a620d8d4ecb9a2ed9edbca8
Tobias Pfeiffer <tgp@preferred.jp>
2015-04-14 13:45:03 +0900

    add basic Context interface and Init(Context) method to Box interface

--- core/box.go
+++ core/box.go
@@ -5,6 +5,7 @@ import (
 )
 
 type Box interface {
+	Init(ctx Context) error
 	Process(t *tuple.Tuple, s Writer) error
 	RequiredInputSchema() ([]*Schema, error)
 	OutputSchema([]*Schema) (*Schema, error)
@@ -14,6 +15,10 @@ type BoxFunc func(t *tuple.Tuple, s Writer) error
 
 func (b *BoxFunc) Process(t *tuple.Tuple, s Writer) error {
 	return (*b)(t, s)
+}
+
+func (b *BoxFunc) Init(ctx Context) error {
+	return nil
 }
 
 func (b *BoxFunc) RequiredInputSchema() ([]*Schema, error) {
