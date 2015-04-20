f6f7fdce6c9c8c89d7128c59728e5119629ef86a
Tobias Pfeiffer <tgp@preferred.jp>
2015-04-20 16:43:52 +0900

    make ConsoleLogger more general and test format of written messages
    fixes #27

--- core/consolelogger.go
+++ core/consolelogger.go
@@ -2,16 +2,35 @@ package core
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"pfi/sensorbee/sensorbee/core/tuple"
 )
 
-type ConsoleLogManager struct{}
-
-func (l *ConsoleLogManager) Log(level LogLevel, msg string, a ...interface{}) {
-	log.Printf("[%-7v] %v", level.String(), fmt.Sprintf(msg, a...))
+// NewConsolePrintLogger creates a LogManager that will write
+// all passed in messages to stderr (just as the normal log
+// modules does).
+func NewConsolePrintLogger() LogManager {
+	// same as log.std logger
+	return NewPrintLogger(os.Stderr)
 }
 
-func (l *ConsoleLogManager) DroppedTuple(t *tuple.Tuple, msg string, a ...interface{}) {
-	log.Printf("[DROPPED] Tuple Batch ID is %v, %v", t.BatchID, fmt.Sprintf(msg, a...))
+// NewPrintLogger creates a LogManager that will write all passed
+// in messages to the given writer.
+func NewPrintLogger(w io.Writer) LogManager {
+	logger := log.New(w, "", log.LstdFlags)
+	return &simplePrintLogger{logger}
+}
+
+type simplePrintLogger struct {
+	logger *log.Logger
+}
+
+func (l *simplePrintLogger) Log(level LogLevel, msg string, a ...interface{}) {
+	l.logger.Printf("[%-7v] %v", level.String(), fmt.Sprintf(msg, a...))
+}
+
+func (l *simplePrintLogger) DroppedTuple(t *tuple.Tuple, msg string, a ...interface{}) {
+	l.logger.Printf("[DROPPED] Tuple Batch ID is %v, %v", t.BatchID, fmt.Sprintf(msg, a...))
 }
