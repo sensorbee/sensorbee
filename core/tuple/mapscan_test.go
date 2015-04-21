3b2d63d5a5785f348d233e8a53956e86ff4c03b0
TANAKA Daisuke <tanakad@preferred.jp>
2015-04-21 17:07:33 +0900

    Modify unit test case

--- core/tuple/mapscan_test.go
+++ core/tuple/mapscan_test.go
@@ -14,49 +14,51 @@ func TestScanMap(t *testing.T) {
 			"nested_string": String("nested hoo"),
 			"nested.string": String("nested loo"),
 		},
-		"string['":       String("funnypath"),
-		"string['aaa":    String("funnypath2"),
-		"string['aaa\"]": String("funnypath3"),
-		"string[\"aaa]":  String("funnypath4"),
 	}
 	Convey("Given a Map with values in it", t, func() {
 		Convey("When accessing a empty string key", func() {
-			_, err := testData.Get("")
+			var v Value
+			err := scanMap(testData, "", &v)
 			Convey("Then lookup should fail", func() {
 				So(err, ShouldNotBeNil)
 				So(err.Error(), ShouldEqual, "empty key is not supported")
 			})
 		})
 		Convey("When accessing an invalid string key", func() {
-			_, err := testData.Get("ab[a")
+			var v Value
+			err := scanMap(testData, "ab[a", &v)
 			Convey("Then lookup should fail", func() {
 				So(err, ShouldNotBeNil)
 				So(err.Error(), ShouldEqual, "invalid path phrase")
 			})
 		})
 		Convey("When accessing a non-existing key", func() {
-			_, err := testData.Get("str")
+			var v Value
+			err := scanMap(testData, "str", &v)
 			Convey("Then lookup should fail", func() {
 				So(err, ShouldNotBeNil)
 				So(err.Error(), ShouldEqual, "not found the key in map: str")
 			})
 		})
 		Convey("When accessing an invalid index in array key", func() {
-			_, err := testData.Get("array[2147483648]")
+			var v Value
+			err := scanMap(testData, "array[2147483648]", &v)
 			Convey("Then lookup should fail", func() {
 				So(err, ShouldNotBeNil)
 				So(err.Error(), ShouldEqual, "overflow index number: array[2147483648]")
 			})
 		})
 		Convey("When accessing an invalid array key", func() {
-			_, err := testData.Get("string[0]")
+			var v Value
+			err := scanMap(testData, "string[0]", &v)
 			Convey("Then lookup should fail", func() {
 				So(err, ShouldNotBeNil)
 				So(err.Error(), ShouldEqual, "unsupported cast array from string")
 			})
 		})
 		Convey("When accessing an out-of-range index", func() {
-			_, err := testData.Get("array[2]")
+			var v Value
+			err := scanMap(testData, "array[2]", &v)
 			Convey("Then lookup should fail", func() {
 				So(err, ShouldNotBeNil)
 				So(err.Error(), ShouldEqual, "out of range access: array[2]")
@@ -64,16 +66,18 @@ func TestScanMap(t *testing.T) {
 		})
 
 		Convey("When accessing nonsense bracket holders", func() {
-			_, err1 := testData.Get(`["string']`)
-			_, err2 := testData.Get(`['string"]`)
+			var x1, x2 Value
+			err1 := scanMap(testData, `["string']`, &x1)
+			err2 := scanMap(testData, `['string"]`, &x2)
 			Convey("Then lookup should fail", func() {
 				So(err1, ShouldNotBeNil)
 				So(err2, ShouldNotBeNil)
 			})
 		})
 		Convey("When accessing bracket holders", func() {
-			x1, err1 := testData.Get(`["string"]`)
-			x2, err2 := testData.Get("['string']")
+			var x1, x2 Value
+			err1 := scanMap(testData, `["string"]`, &x1)
+			err2 := scanMap(testData, "['string']", &x2)
 			Convey("Then lookup should exist", func() {
 				So(err1, ShouldBeNil)
 				So(err2, ShouldBeNil)
@@ -86,11 +90,12 @@ func TestScanMap(t *testing.T) {
 			})
 		})
 		Convey("When accessing nested bracket holders", func() {
-			x1, err1 := testData.Get(`["map"]["nested string"]`)
-			x2, err2 := testData.Get(`map['nested.string']`)
-			x3, err3 := testData.Get(`["map"]nested_string`)
-			x4, err4 := testData.Get(`map.['nested.string']`)
-			x5, err5 := testData.Get(`["map"].nested_string`)
+			var x1, x2, x3, x4, x5 Value
+			err1 := scanMap(testData, `["map"]["nested string"]`, &x1)
+			err2 := scanMap(testData, `map['nested.string']`, &x2)
+			err3 := scanMap(testData, `["map"]nested_string`, &x3)
+			err4 := scanMap(testData, `map.['nested.string']`, &x4)
+			err5 := scanMap(testData, `["map"].nested_string`, &x5)
 			Convey("Then lookup should exist", func() {
 				So(err1, ShouldBeNil)
 				So(err2, ShouldBeNil)
