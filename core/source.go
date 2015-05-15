16c3953bdb4d60f52b2836275609dfe82dd67c80
Tobias Pfeiffer <tgp@preferred.jp>
2015-05-15 16:24:50 +0900

    restyle interface/struct documentation as per the style guide

--- core/source.go
+++ core/source.go
@@ -2,23 +2,22 @@ package core
 
 // A Source describes an entity that inserts data into a topology
 // from the outside world (e.g., a fluentd instance).
-//
-// GenerateStream will start creating tuples and writing them to
-// the given Writer in a blocking way. (Start as a goroutine to start
-// the process in the background.) It will return when all tuples
-// have been written (in the case of a finite data source) or if
-// there was a severe error. The context that is passed in will be
-// used as a parameter to the Write method of the given Writer.
-//
-// Schema will return the schema of the data that can be expected
-// from this Source. It is nil if no guarantees about that schema
-// are made.
 type Source interface {
+	// GenerateStream will start creating tuples and writing them to
+	// the given Writer in a blocking way. (Start as a goroutine to start
+	// the process in the background.) It will return when all tuples
+	// have been written (in the case of a finite data source) or if
+	// there was a severe error. The context that is passed in will be
+	// used as a parameter to the Write method of the given Writer.
 	GenerateStream(ctx *Context, w Writer) error
 
 	// Stop will tell the Source to stop emitting tuples. After this
 	// function returns, no more calls to Write shall be made on the
 	// Writer passed in to GenerateStream.
 	Stop(ctx *Context) error
+
+	// Schema will return the schema of the data that can be expected
+	// from this Source. It is nil if no guarantees about that schema
+	// are made.
 	Schema() *Schema
 }
