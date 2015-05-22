b867f935b52014311ece2cb575f44a3fea59755d
Tobias Pfeiffer <tgp@preferred.jp>
2015-05-22 10:32:58 +0900

    make RangeUnit typesafe

--- bql/parser/assemble_range_test.go
+++ bql/parser/assemble_range_test.go
@@ -12,7 +12,7 @@ func TestAssembleRange(t *testing.T) {
 		Convey("When the stack contains two correct items", func() {
 			ps.PushComponent(0, 6, Raw{"PRE"})
 			ps.PushComponent(6, 7, Raw{"2"})
-			ps.PushComponent(7, 8, RangeUnit{"SECONDS"})
+			ps.PushComponent(7, 8, Seconds)
 			ps.AssembleRange()
 
 			Convey("Then AssembleRange replaces them with a new item", func() {
@@ -28,7 +28,7 @@ func TestAssembleRange(t *testing.T) {
 					Convey("And it contains the previous data", func() {
 						comp := top.comp.(Range)
 						So(comp.expr, ShouldEqual, "2")
-						So(comp.unit, ShouldEqual, "SECONDS")
+						So(comp.unit, ShouldEqual, Seconds)
 					})
 				})
 			})
