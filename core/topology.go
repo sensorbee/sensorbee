a0771f048328826f4976c9fc90ff0050054dbfa1
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-06-08 14:53:46 -0700

    Add input config types for Box and Sink

--- core/topology.go
+++ core/topology.go
@@ -23,3 +23,53 @@ const (
 	// have to be able to run again.
 	TSStopped
 )
+
+// BoxInputConfig has parameters to customize input behavior of a Box on each
+// input pipe.
+type BoxInputConfig struct {
+	// InputName is a custom name attached to incoming tuples. When it is empty,
+	// "*" will be used.
+	InputName string
+
+	// Capacity is the maximum capacity (length) of input pipe. When this
+	// parameter is 0, the default value is used. This parameter is only used
+	// as a hint and doesn't guarantee that the pipe can actually have the
+	// specified number of tuples.
+	Capacity int
+}
+
+func (c *BoxInputConfig) inputName() string {
+	if c.InputName == "" {
+		return "*"
+	}
+	return c.InputName
+}
+
+func (c *BoxInputConfig) capacity() int {
+	if c.Capacity == 0 {
+		return 1024
+	}
+	return c.Capacity
+}
+
+// SinkInputConfig has parameters to customize input behavior of a Sink on
+// each input pipe.
+type SinkInputConfig struct {
+	// Capacity is the maximum capacity (length) of input pipe. When this
+	// parameter is 0, the default value is used. This parameter is only used
+	// as a hint and doesn't guarantee that the pipe can actually have the
+	// specified number of tuples.
+	Capacity int
+}
+
+func (c *SinkInputConfig) capacity() int {
+	if c.Capacity == 0 {
+		return 1024
+	}
+	return c.Capacity
+}
+
+var (
+	defaultBoxInputConfig  = &BoxInputConfig{}
+	defaultSinkInputConfig = &SinkInputConfig{}
+)
