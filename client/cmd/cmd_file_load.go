5c88277a05e7f040e370f941e3c65ce54f65b7bd
TANAKA Daisuke <tanakad@preferred.jp>
2015-06-23 11:26:59 -0700

    add "file" command

---
+++ client/cmd/cmd_file_load.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func NewFileLoadCommands() []Command {
+	return []Command{
+		&fileLoadCmd{},
+	}
+}
+
+type fileLoadCmd struct {
+	queries string
+}
+
+func (f *fileLoadCmd) Init() error {
+	return nil
+}
+
+func (f *fileLoadCmd) Name() []string {
+	return []string{"file"}
+}
+
+func (f *fileLoadCmd) Input(input string) (cmdInputStatusType, error) {
+	inputs := strings.Split(input, " ")
+	if len(inputs) != 2 {
+		return invalidCMD, fmt.Errorf("cannot read file name")
+	}
+	filePath := inputs[1]
+
+	// file load
+	queries, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return invalidCMD, fmt.Errorf("cannot read queries from file: %v", filePath)
+	}
+	f.queries = string(queries)
+	return preparedCMD, nil
+}
+
+func (f *fileLoadCmd) Eval() (RequestType, string, interface{}) {
+	uri := topologiesHeader + "/" + currentTopology.name + "/queries"
+	m := map[string]interface{}{}
+	m["queries"] = f.queries
+	return PostRequest, uri, &m
+}
