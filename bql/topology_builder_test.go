0f7c42b00f8d6f72fdd381e76a423869af5a2c5d
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-06-24 22:27:52 -0700

    Remove CreateStreamFromSourceStmt and CreateStreamFromSourceExtStmt

--- bql/topology_builder_test.go
+++ bql/topology_builder_test.go
@@ -76,112 +76,15 @@ func TestCreateSourceStmt(t *testing.T) {
 	})
 }
 
-func TestCreateStreamFromSourceStmt(t *testing.T) {
-	Convey("Given a BQL TopologyBuilder with a source", t, func() {
-		dt := newTestDynamicTopology()
-		Reset(func() {
-			unblockSource(dt, "hoge_src")
-			dt.Stop()
-		})
-		tb := bql.NewTopologyBuilder(dt)
-		err := addBQLToTopology(tb, `CREATE SOURCE hoge TYPE blocking_dummy`)
-		So(err, ShouldBeNil)
-
-		Convey("When running CREATE STREAM FROM SOURCE on an existing source", func() {
-			err := addBQLToTopology(tb, `CREATE STREAM s FROM SOURCE hoge`)
-
-			Convey("Then there should be no error", func() {
-				So(err, ShouldBeNil)
-			})
-
-			Convey("And when running another CREATE STREAM FROM SOURCE with the same name", func() {
-				err := addBQLToTopology(tb, `CREATE STREAM s FROM SOURCE hoge`)
-
-				Convey("Then an error should be returned the second time", func() {
-					So(err, ShouldNotBeNil)
-					So(err.Error(), ShouldContainSubstring, "already")
-				})
-			})
-
-			Convey("And when running another CREATE STREAM FROM SOURCE with a different name", func() {
-				err := addBQLToTopology(tb, `CREATE STREAM t FROM SOURCE hoge`)
-
-				Convey("Then there should be no error", func() {
-					So(err, ShouldBeNil)
-				})
-			})
-		})
-
-		Convey("When running CREATE STREAM FROM SOURCE on a non-existing source", func() {
-			err := addBQLToTopology(tb, `CREATE STREAM s FROM SOURCE foo`)
-
-			Convey("Then an error should be returned", func() {
-				So(err, ShouldNotBeNil)
-				So(err.Error(), ShouldContainSubstring, "was not found")
-			})
-		})
-
-		Convey("When running CREATE STREAM FROM SOURCE with stream and source name the same", func() {
-			err := addBQLToTopology(tb, `CREATE STREAM hoge FROM SOURCE hoge`)
-
-			Convey("Then there should be no error", func() {
-				So(err, ShouldBeNil)
-			})
-		})
-	})
-}
-
-func TestCreateStreamFromSourceExtStmt(t *testing.T) {
-	Convey("Given a BQL TopologyBuilder", t, func() {
-		dt := newTestDynamicTopology()
-		Reset(func() {
-			dt.Stop()
-		})
-		tb := bql.NewTopologyBuilder(dt)
-
-		Convey("When running CREATE STREAM FROM SOURCE (combo) with a dummy source", func() {
-			err := addBQLToTopology(tb, `CREATE STREAM hoge FROM dummy SOURCE`)
-			So(err, ShouldBeNil)
-
-			Convey("And when running another CREATE STREAM FROM SOURCE (combo) using the same name", func() {
-				err := addBQLToTopology(tb, `CREATE STREAM hoge FROM dummy SOURCE`)
-
-				Convey("Then an error should be returned", func() {
-					So(err, ShouldNotBeNil)
-					So(err.Error(), ShouldContainSubstring, "already")
-				})
-			})
-		})
-		Convey("When running CREATE STREAM FROM SOURCE (combo) with invalid parameters", func() {
-			err := addBQLToTopology(tb, `CREATE STREAM hoge FROM dummy SOURCE WITH foo='bar'`)
-
-			Convey("Then an error should be returned", func() {
-				So(err, ShouldNotBeNil)
-				So(err.Error(), ShouldEqual, "unknown source parameter: foo")
-			})
-		})
-		Convey("When running CREATE STREAM FROM SOURCE (combo) with an unknown source type", func() {
-			err := addBQLToTopology(tb, `CREATE STREAM hoge FROM foo SOURCE`)
-
-			Convey("Then an error should be returned", func() {
-				So(err, ShouldNotBeNil)
-				So(err.Error(), ShouldEqual, "unknown source type: foo")
-			})
-		})
-	})
-}
-
 func TestCreateStreamAsSelectStmt(t *testing.T) {
 	Convey("Given a BQL TopologyBuilder with source and stream", t, func() {
 		dt := newTestDynamicTopology()
 		Reset(func() {
-			unblockSource(dt, "hoge_src")
+			unblockSource(dt, "s")
 			dt.Stop()
 		})
 		tb := bql.NewTopologyBuilder(dt)
-		err := addBQLToTopology(tb, `CREATE SOURCE hoge TYPE blocking_dummy`)
-		So(err, ShouldBeNil)
-		err = addBQLToTopology(tb, `CREATE STREAM s FROM SOURCE hoge`)
+		err := addBQLToTopology(tb, `CREATE SOURCE s TYPE blocking_dummy`)
 		So(err, ShouldBeNil)
 
 		Convey("When running CREATE STREAM AS SELECT on an existing stream", func() {
@@ -209,16 +112,6 @@ func TestCreateStreamAsSelectStmt(t *testing.T) {
 				Convey("Then there should be no error", func() {
 					So(err, ShouldBeNil)
 				})
-			})
-		})
-
-		Convey("When running CREATE STREAM AS SELECT on a source name", func() {
-			err := addBQLToTopology(tb, `CREATE STREAM t AS SELECT ISTREAM int FROM
-                hoge [RANGE 2 SECONDS] WHERE int=2`)
-
-			Convey("Then an error should be returned", func() {
-				So(err, ShouldNotBeNil)
-				So(err.Error(), ShouldContainSubstring, "was not found")
 			})
 		})
 
@@ -291,13 +184,11 @@ func TestInsertIntoSelectStmt(t *testing.T) {
 	Convey("Given a BQL TopologyBuilder with source, stream, and sink", t, func() {
 		dt := newTestDynamicTopology()
 		Reset(func() {
-			unblockSource(dt, "hoge_src")
+			unblockSource(dt, "s")
 			dt.Stop()
 		})
 		tb := bql.NewTopologyBuilder(dt)
-		err := addBQLToTopology(tb, `CREATE SOURCE hoge TYPE blocking_dummy`)
-		So(err, ShouldBeNil)
-		err = addBQLToTopology(tb, `CREATE STREAM s FROM SOURCE hoge`)
+		err := addBQLToTopology(tb, `CREATE SOURCE s TYPE blocking_dummy`)
 		So(err, ShouldBeNil)
 		err = addBQLToTopology(tb, `CREATE SINK foo TYPE collector`)
 		So(err, ShouldBeNil)
@@ -357,7 +248,7 @@ func TestMultipleStatements(t *testing.T) {
 
 		Convey("When issuing multiple commands in a good order", func() {
 			stmts := `
-			CREATE STREAM source FROM blocking_dummy SOURCE WITH num=4;
+			CREATE SOURCE source TYPE blocking_dummy WITH num=4;
 			CREATE STREAM box AS SELECT
 			  ISTREAM int, str((int+1) % 3) AS x FROM source
 			  [RANGE 2 SECONDS] WHERE int % 2 = 0;
@@ -374,7 +265,7 @@ func TestMultipleStatements(t *testing.T) {
 
 		Convey("When issuing multiple commands in a bad order", func() {
 			stmts := `
-			CREATE STREAM source FROM blocking_dummy SOURCE WITH num=4;
+			CREATE SOURCE source TYPE blocking_dummy WITH num=4;
 			CREATE STREAM box AS SELECT
 			  ISTREAM int, str((int+1) % 3) AS x FROM source
 			  [RANGE 2 SECONDS] WHERE int % 2 = 0;
