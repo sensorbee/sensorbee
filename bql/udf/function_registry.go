032a154ca08c9419925810176a08baab56249b5f
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-06-24 23:21:20 -0700

    Provide udf.UDF interface for extensibility
    Also, remove some methods from Manager.

--- bql/udf/function_registry.go
+++ bql/udf/function_registry.go
@@ -7,8 +7,93 @@ import (
 	"sync"
 )
 
-// VarParamFun is an alias for a variadic function on tuple.Value.
-type VarParamFun func(*core.Context, ...tuple.Value) (tuple.Value, error)
+// UDF is an interface having a user defined function.
+type UDF interface {
+	// Call calls the UDF.
+	Call(*core.Context, ...tuple.Value) (tuple.Value, error)
+
+	// Accept checks if the function accepts the given number of arguments
+	// excluding core.Context.
+	Accept(arity int) bool
+}
+
+type function struct {
+	f     func(*core.Context, ...tuple.Value) (tuple.Value, error)
+	arity int
+}
+
+func (f *function) Call(ctx *core.Context, args ...tuple.Value) (tuple.Value, error) {
+	return f.f(ctx, args...)
+}
+
+func (f *function) Accept(arity int) bool {
+	if f.arity < 0 {
+		return true
+	}
+	return arity == f.arity
+}
+
+// VariadicFunc creates a UDF based on a function receiving the variadic number
+// of tuple.Values.
+func VariadicFunc(f func(*core.Context, ...tuple.Value) (tuple.Value, error)) UDF {
+	return &function{
+		f:     f,
+		arity: -1,
+	}
+}
+
+func Func(f func(*core.Context, ...tuple.Value) (tuple.Value, error), arity int) UDF {
+	return &function{
+		f:     f,
+		arity: arity,
+	}
+}
+
+func NullaryFunc(f func(*core.Context) (tuple.Value, error)) UDF {
+	genFunc := func(ctx *core.Context, vs ...tuple.Value) (tuple.Value, error) {
+		if len(vs) != 0 {
+			return nil, fmt.Errorf("the function should be used as nullary")
+		}
+		return f(ctx)
+	}
+	return Func(genFunc, 0)
+}
+
+func UnaryFunc(f func(*core.Context, tuple.Value) (tuple.Value, error)) UDF {
+	genFunc := func(ctx *core.Context, vs ...tuple.Value) (tuple.Value, error) {
+		if len(vs) != 1 {
+			return nil, fmt.Errorf("the function should be used as unary")
+		}
+		return f(ctx, vs[0])
+	}
+	return Func(genFunc, 1)
+}
+
+func BinaryFunc(f func(*core.Context, tuple.Value, tuple.Value) (tuple.Value, error)) UDF {
+	genFunc := func(ctx *core.Context, vs ...tuple.Value) (tuple.Value, error) {
+		if len(vs) != 2 {
+			return nil, fmt.Errorf("the function should be used as binary")
+		}
+		return f(ctx, vs[0], vs[1])
+	}
+	return Func(genFunc, 2)
+}
+
+func TernaryFunc(f func(*core.Context, tuple.Value, tuple.Value, tuple.Value) (tuple.Value, error)) UDF {
+	genFunc := func(ctx *core.Context, vs ...tuple.Value) (tuple.Value, error) {
+		if len(vs) != 3 {
+			return nil, fmt.Errorf("the function should be used as ternary")
+		}
+		return f(ctx, vs[0], vs[1], vs[2])
+	}
+	return Func(genFunc, 3)
+}
+
+// TODO: Add magic UDF generator func NewUDF(f interface{}) (UDF, error)
+//       It accepts any function whose arguments are convertible to tuple.Value.
+//       For example, NewUDF(func(*core.Context, a, b int) (int, error) {return a + b}).
+//       Even NewUDF(func(a, b int) int {return a + b}) could be valid
+//       (i.e. Context and error can be optional)
 
 // FunctionRegistry is an interface to lookup functions for use in BQL
 // statements by their name.
@@ -22,7 +107,7 @@ type FunctionRegistry interface {
 	// and it also allows functions with an arbitrary number of
 	// parameters. However, a function returned must never be used
 	// with a different arity than the one given in the Lookup call.
-	Lookup(name string, arity int) (VarParamFun, error)
+	Lookup(name string, arity int) (UDF, error)
 }
 
 // FunctionManager is a FunctionRegistry that allows to register
@@ -30,37 +115,26 @@ type FunctionRegistry interface {
 type FunctionManager interface {
 	FunctionRegistry
 
-	// Register allows to add a function that works exactly with
-	// the given number of parameters.
-	Register(name string, f VarParamFun, arity int) error
-
-	// RegisterVariadic allows to add a variadic function.
-	RegisterVariadic(name string, f VarParamFun) error
+	// Register allows to add a function.
+	Register(name string, f UDF) error
 }
 
 type defaultFunctionRegistry struct {
 	ctx   *core.Context
 	m     sync.RWMutex
-	funcs map[string]struct {
-		fun     VarParamFun
-		checker func(int) bool
-	}
+	funcs map[string]UDF
 }
 
-func NewDefaultFunctionRegistry(ctx *core.Context) *defaultFunctionRegistry {
-	empty := map[string]struct {
-		fun     VarParamFun
-		checker func(int) bool
-	}{}
+func NewDefaultFunctionRegistry(ctx *core.Context) FunctionManager {
 	reg := &defaultFunctionRegistry{
 		ctx:   ctx,
-		funcs: empty,
+		funcs: map[string]UDF{},
 	}
 	// register some standard functions
 	toString := func(ctx *core.Context, v tuple.Value) (tuple.Value, error) {
 		return tuple.String(v.String()), nil
 	}
-	reg.RegisterUnary("str", toString)
+	reg.Register("str", UnaryFunc(toString))
 	return reg
 }
 
@@ -68,83 +142,24 @@ func (fr *defaultFunctionRegistry) Context() *core.Context {
 	return fr.ctx
 }
 
-func (fr *defaultFunctionRegistry) Lookup(name string, arity int) (VarParamFun, error) {
+func (fr *defaultFunctionRegistry) Lookup(name string, arity int) (UDF, error) {
 	fr.m.RLock()
 	defer fr.m.RUnlock()
-	// look for variable-parameter functions
-	funWithChecker, exists := fr.funcs[name]
-	if exists {
-		if funWithChecker.checker(arity) {
-			return funWithChecker.fun, nil
+	if f, exists := fr.funcs[name]; exists {
+		if f.Accept(arity) {
+			return f, nil
 		}
 		return nil, fmt.Errorf("function '%s' is not %d-ary", name, arity)
 	}
 	return nil, fmt.Errorf("function '%s' is unknown", name)
 }
 
-func (fr *defaultFunctionRegistry) Register(name string, f VarParamFun, arity int) error {
-	return fr.registerFlexible(name, f, func(i int) bool {
-		return i == arity
-	})
-}
-
-func (fr *defaultFunctionRegistry) RegisterVariadic(name string, f VarParamFun) error {
-	return fr.registerFlexible(name, f, func(i int) bool {
-		return true
-	})
-}
-
-func (fr *defaultFunctionRegistry) registerFlexible(name string, f VarParamFun, arityOk func(int) bool) error {
+func (fr *defaultFunctionRegistry) Register(name string, f UDF) error {
 	fr.m.Lock()
 	defer fr.m.Unlock()
-	_, exists := fr.funcs[name]
-	if exists {
+	if _, exists := fr.funcs[name]; exists {
 		return fmt.Errorf("there is already a function named '%s'", name)
 	}
-	newData := struct {
-		fun     VarParamFun
-		checker func(int) bool
-	}{f, arityOk}
-	fr.funcs[name] = newData
+	fr.funcs[name] = f
 	return nil
-}
-
-func (fr *defaultFunctionRegistry) RegisterNullary(name string, f func(*core.Context) (tuple.Value, error)) error {
-	genFunc := func(ctx *core.Context, vs ...tuple.Value) (tuple.Value, error) {
-		if len(vs) != 0 {
-			return nil, fmt.Errorf("function '%s' should be used as nullary", name)
-		}
-		return f(ctx)
-	}
-	return fr.Register(name, genFunc, 0)
-}
-
-func (fr *defaultFunctionRegistry) RegisterUnary(name string, f func(*core.Context, tuple.Value) (tuple.Value, error)) error {
-	genFunc := func(ctx *core.Context, vs ...tuple.Value) (tuple.Value, error) {
-		if len(vs) != 1 {
-			return nil, fmt.Errorf("function '%s' should be used as unary", name)
-		}
-		return f(ctx, vs[0])
-	}
-	return fr.Register(name, genFunc, 1)
-}
-
-func (fr *defaultFunctionRegistry) RegisterBinary(name string, f func(*core.Context, tuple.Value, tuple.Value) (tuple.Value, error)) error {
-	genFunc := func(ctx *core.Context, vs ...tuple.Value) (tuple.Value, error) {
-		if len(vs) != 2 {
-			return nil, fmt.Errorf("function '%s' should be used as binary", name)
-		}
-		return f(ctx, vs[0], vs[1])
-	}
-	return fr.Register(name, genFunc, 2)
-}
-
-func (fr *defaultFunctionRegistry) RegisterTernary(name string, f func(*core.Context, tuple.Value, tuple.Value, tuple.Value) (tuple.Value, error)) error {
-	genFunc := func(ctx *core.Context, vs ...tuple.Value) (tuple.Value, error) {
-		if len(vs) != 3 {
-			return nil, fmt.Errorf("function '%s' should be used as ternary", name)
-		}
-		return f(ctx, vs[0], vs[1], vs[2])
-	}
-	return fr.Register(name, genFunc, 3)
 }
