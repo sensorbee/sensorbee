9290d7878286a8777779ba8443914642a7f8efdf
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-07-07 20:03:49 -0700

    Simplify the template of build_sensorbee

--- cmd/sensorbee/default_main.go
+++ cmd/sensorbee/default_main.go
@@ -9,54 +9,22 @@ import (
 	"time"
 )
 
-type commandGenerator func() cli.Command
-
 func init() {
 	// TODO
 	time.Local = time.UTC
 }
 
 func main() {
-	app := setUpApp([]commandGenerator{
-		run.SetUp,
-		shell.SetUp,
-		topology.SetUp,
-	})
-
-	if err := app.Run(os.Args); err != nil {
-		os.Exit(1)
-	}
-}
-
-func setUpApp(cmds []commandGenerator) *cli.App {
 	app := cli.NewApp()
 	app.Name = "sensorbee"
 	app.Usage = "SenserBee"
 	app.Version = "0.0.1" // TODO get dynamic, will be get from external file
-	app.Flags = []cli.Flag{
-		cli.StringFlag{ // TODO get configuration from external file
-			Name:   "config, c",
-			Value:  "/etc/sersorbee/sensorbee.config",
-			Usage:  "path to the config file",
-			EnvVar: "SENSORBEE_CONFIG",
-		},
+	app.Commands = []cli.Command{
+		run.SetUp(),
+		shell.SetUp(),
+		topology.SetUp(),
 	}
-	app.Before = appBeforeHook
-
-	for _, c := range cmds {
-		app.Commands = append(app.Commands, c())
+	if err := app.Run(os.Args); err != nil {
+		os.Exit(1)
 	}
-	return app
-}
-
-func appBeforeHook(c *cli.Context) error {
-	if err := loadConfig(c); err != nil {
-		return err
-	}
-	return nil
-}
-
-func loadConfig(c *cli.Context) error {
-	// TODO load configuration file (YAML)
-	return nil
 }
