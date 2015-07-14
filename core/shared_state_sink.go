1726d0b31023cca4df00b644c1a0d705f61b8de9
Shuzo Kashihara <kashihara@preferred.jp>
2015-07-14 18:58:51 +0900

    Add core.NewSharedStateSink and register sink as 'uds'

---
+++ core/shared_state_sink.go
@@ -0,0 +1,43 @@
+package core
+
+import (
+	"fmt"
+)
+
+type sharedStateSink struct {
+	writer Writer
+}
+
+// NewSharedStateSink creates a sink that writes to SharedState.
+func NewSharedStateSink(ctx *Context, name string) (Sink, error) {
+	registry := ctx.SharedStates
+
+	// Get SharedState by name
+	state, err := registry.Get(name)
+	if err != nil {
+		return nil, err
+	}
+
+	// It fails if the shared state cannot be written
+	writer, ok := state.(Writer)
+	if !ok {
+		return nil, fmt.Errorf("'%v' state cannot be written")
+	}
+
+	s := &sharedStateSink{
+		writer: writer,
+	}
+	return s, nil
+}
+
+func (s *sharedStateSink) Write(ctx *Context, t *Tuple) error {
+	return s.writer.Write(ctx, t)
+}
+
+func (s *sharedStateSink) Close(ctx *Context) error {
+	closer, ok := s.writer.(WriteCloser)
+	if !ok {
+		return nil
+	}
+	return closer.Close(ctx)
+}
