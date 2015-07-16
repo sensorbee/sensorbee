4b6156fe5a2e175af8940b8f9a42dcc5d4324cc5
Tobias Pfeiffer <tgp@preferred.jp>
2015-07-16 18:07:30 +0900

    add the first functions to the standard library
    abs, cbrt, ceil, degrees, exp, floor

---
+++ bql/udf/builtin_numeric.go
@@ -0,0 +1,157 @@
+package udf
+
+import (
+	"fmt"
+	"math"
+	"pfi/sensorbee/sensorbee/core"
+	"pfi/sensorbee/sensorbee/data"
+)
+
+// singleParamFunc is a template for functions that
+// have exactly one parameter
+type singleParamFunc struct {
+}
+
+func (f *singleParamFunc) Accept(arity int) bool {
+	return arity == 1
+}
+
+func (f *singleParamFunc) IsAggregationParameter(k int) bool {
+	return false
+}
+
+// typePreservingSingleParamNumericFunc is a template for
+// numeric functions that have the same return type as
+// input type. If intFun is nil, then the result is computed
+// by converting input and output of floatFun.
+type typePreservingSingleParamNumericFunc struct {
+	singleParamFunc
+	intFun   func(int64) int64
+	floatFun func(float64) float64
+}
+
+func (f *typePreservingSingleParamNumericFunc) Call(ctx *core.Context, args ...data.Value) (data.Value, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("function takes exactly one argument")
+	}
+	arg := args[0]
+	if arg.Type() == data.TypeNull {
+		return data.Null{}, nil
+	} else if arg.Type() == data.TypeInt && f.intFun == nil {
+		i, _ := data.AsInt(arg)
+		return data.Int(f.floatFun(float64(i))), nil
+	} else if arg.Type() == data.TypeInt {
+		i, _ := data.AsInt(arg)
+		return data.Int(f.intFun(i)), nil
+	} else if arg.Type() == data.TypeFloat {
+		d, _ := data.AsFloat(arg)
+		return data.Float(f.floatFun(d)), nil
+	}
+	return nil, fmt.Errorf("cannot interpret %s as number", arg)
+}
+
+// floatValuedSingleParamNumericFunc is a template for
+// numeric functions that return a floating point value
+// even if the input is integral
+type floatValuedSingleParamNumericFunc struct {
+	singleParamFunc
+	floatFun func(float64) float64
+}
+
+func (f *floatValuedSingleParamNumericFunc) Call(ctx *core.Context, args ...data.Value) (data.Value, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("function takes exactly one argument")
+	}
+	arg := args[0]
+	if arg.Type() == data.TypeNull {
+		return data.Null{}, nil
+	} else if arg.Type() == data.TypeInt {
+		i, _ := data.AsInt(arg)
+		return data.Float(f.floatFun(float64(i))), nil
+	} else if arg.Type() == data.TypeFloat {
+		d, _ := data.AsFloat(arg)
+		return data.Float(f.floatFun(d)), nil
+	}
+	return nil, fmt.Errorf("cannot interpret %s as number", arg)
+}
+
+// AbsFunc returns a UDF to compute the absolute value of a number.
+// See also: math.Abs.
+//
+// That UDF can be used in BQL as `abs`.
+//
+//  Input: Int or Float
+//  Return Type: same as input
+func AbsFunc() UDF {
+	return &typePreservingSingleParamNumericFunc{
+		floatFun: math.Abs,
+	}
+}
+
+// CbrtFunc returns a UDF to compute the cube root of a number.
+// See also: math.Cbrt.
+//
+// That UDF can be used in BQL as `cbrt`.
+//
+//  Input: Int or Float
+//  Return Type: Float
+func CbrtFunc() UDF {
+	return &floatValuedSingleParamNumericFunc{
+		floatFun: math.Cbrt,
+	}
+}
+
+// CeilFunc returns a UDF to compute the smallest integer not less
+// than its argument. See also: math.Ceil.
+//
+// That UDF can be used in BQL as `ceil`.
+//
+//  Input: Int or Float
+//  Return Type: same as input
+func CeilFunc() UDF {
+	return &typePreservingSingleParamNumericFunc{
+		floatFun: math.Ceil,
+	}
+}
+
+// DegreesFunc returns a UDF to convert radians to degrees.
+//
+// That UDF can be used in BQL as `degrees`.
+//
+//  Input: Int or Float
+//  Return Type: Float
+func DegreesFunc() UDF {
+	return &floatValuedSingleParamNumericFunc{
+		floatFun: func(f float64) float64 {
+			return f / math.Pi * 180
+		},
+	}
+}
+
+// TODO DivFunc
+
+// ExpFunc returns a UDF to compute the exponential of a number.
+// See also: math.Exp.
+//
+// That UDF can be used in BQL as `exp`.
+//
+//  Input: Int or Float
+//  Return Type: Float
+func ExpFunc() UDF {
+	return &floatValuedSingleParamNumericFunc{
+		floatFun: math.Exp,
+	}
+}
+
+// FloorFunc returns a UDF to compute the largest integer not greater
+// than its argument. See also: math.Floor.
+//
+// That UDF can be used in BQL as `ceil`.
+//
+//  Input: Int or Float
+//  Return Type: same as input
+func FloorFunc() UDF {
+	return &typePreservingSingleParamNumericFunc{
+		floatFun: math.Floor,
+	}
+}
---
+++ bql/udf/builtin_numeric_test.go
@@ -0,0 +1,109 @@
+package udf
+
+import (
+	"fmt"
+	. "github.com/smartystreets/goconvey/convey"
+	"math"
+	"pfi/sensorbee/sensorbee/data"
+	"testing"
+	"time"
+)
+
+type udfTestCase struct {
+	name   string
+	udf    UDF
+	inputs []udfTestCaseInput
+}
+
+type udfTestCaseInput struct {
+	input    data.Value
+	expected data.Value
+}
+
+func TestNumericFuncs(t *testing.T) {
+	someTime := time.Date(2015, time.May, 1, 14, 27, 0, 0, time.UTC)
+
+	invalidInputs := []udfTestCaseInput{
+		// NULL input -> NULL output
+		{data.Null{}, data.Null{}},
+		// cannot process the following
+		{data.Array{}, nil},
+		{data.Blob{}, nil},
+		{data.Bool(true), nil},
+		{data.Map{}, nil},
+		{data.String("hoge"), nil},
+		{data.Timestamp(someTime), nil},
+	}
+
+	udfTestCases := []udfTestCase{
+		{"abs", AbsFunc(), []udfTestCaseInput{
+			{data.Int(7), data.Int(7)},
+			{data.Int(-7), data.Int(7)},
+			{data.Float(2.3), data.Float(2.3)},
+			{data.Float(-2.3), data.Float(2.3)},
+		}},
+		{"cbrt", CbrtFunc(), []udfTestCaseInput{
+			{data.Int(27), data.Float(3.0)},
+			{data.Int(-27), data.Float(-3.0)},
+			{data.Float(27.0), data.Float(3.0)},
+			{data.Float(-27.0), data.Float(-3.0)},
+		}},
+		{"ceil", CeilFunc(), []udfTestCaseInput{
+			{data.Int(27), data.Int(27)},
+			{data.Int(-27), data.Int(-27)},
+			{data.Float(42.8), data.Float(43.0)},
+			{data.Float(-42.8), data.Float(-42.0)},
+		}},
+		{"degrees", DegreesFunc(), []udfTestCaseInput{
+			{data.Int(1), data.Float(57.29577951308232)},
+			{data.Float(0.5), data.Float(28.6478897565412)},
+		}},
+		{"exp", ExpFunc(), []udfTestCaseInput{
+			{data.Int(1), data.Float(math.E)},
+			{data.Float(math.Log(1.0)), data.Float(1.0)},
+		}},
+		{"floor", FloorFunc(), []udfTestCaseInput{
+			{data.Int(27), data.Int(27)},
+			{data.Int(-27), data.Int(-27)},
+			{data.Float(42.8), data.Float(42.0)},
+			{data.Float(-42.8), data.Float(-43.0)},
+		}},
+	}
+
+	for _, udfTestCase := range udfTestCases {
+		udfTestCase := udfTestCase
+		udf := udfTestCase.udf
+		testCases := append(udfTestCase.inputs, invalidInputs...)
+
+		Convey(fmt.Sprintf("Given the %s function", udfTestCase.name), t, func() {
+			for _, tc := range testCases {
+				tc := tc
+
+				Convey(fmt.Sprintf("When evaluating it on %s (%T)", tc.input, tc.input), func() {
+					val, err := udf.Call(nil, tc.input)
+
+					if tc.expected == nil {
+						Convey("Then evaluation should fail", func() {
+							So(err, ShouldNotBeNil)
+						})
+					} else {
+						Convey(fmt.Sprintf("Then the result should be %s", tc.expected), func() {
+							So(err, ShouldBeNil)
+							if val.Type() == data.TypeFloat && tc.expected.Type() == data.TypeFloat {
+								So(val, ShouldAlmostEqual, tc.expected, 0.0000001)
+							} else {
+								So(val, ShouldResemble, tc.expected)
+							}
+						})
+					}
+				})
+			}
+
+			Convey("Then it should equal in the default registry", func() {
+				regFun, err := globalUDFRegistry.Lookup(udfTestCase.name, 1)
+				So(err, ShouldBeNil)
+				So(regFun, ShouldHaveSameTypeAs, udf)
+			})
+		})
+	}
+}
