4961ad26efc74d53d0688e5553211bf058983197
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-07-21 15:47:04 -0700

    Add sources controller

---
+++ server/response/source.go
@@ -0,0 +1,31 @@
+package response
+
+import (
+	"pfi/sensorbee/sensorbee/core"
+	"pfi/sensorbee/sensorbee/data"
+)
+
+// Source is a part of the response which is returned by sources' action.
+type Source struct {
+	NodeType string      `json:"node_type"`
+	Name     string      `json:"name"`
+	State    string      `json:"state"`
+	Status   data.Map    `json:"status,omitempty"`
+	Meta     interface{} `json:"meta,omitempty"`
+}
+
+// NewSource returns the result of the source node. It generates status and
+// meta information if detailed argument is true.
+func NewSource(sn core.SourceNode, detailed bool) *Source {
+	s := &Source{
+		NodeType: core.NTSource.String(),
+		Name:     sn.Name(),
+		State:    sn.State().Get().String(),
+	}
+
+	if detailed {
+		s.Status = sn.Status()
+		s.Meta = sn.Meta()
+	}
+	return s
+}
