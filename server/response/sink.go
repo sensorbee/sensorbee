5d3bcc0d94fd4b0e11b1d6ca0f6df087bebb112c
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-07-21 16:27:43 -0700

    Add sinks controller

---
+++ server/response/sink.go
@@ -0,0 +1,31 @@
+package response
+
+import (
+	"pfi/sensorbee/sensorbee/core"
+	"pfi/sensorbee/sensorbee/data"
+)
+
+// Sink is a part of the response which is returned by sinks' action.
+type Sink struct {
+	NodeType string      `json:"node_type"`
+	Name     string      `json:"name"`
+	State    string      `json:"state"`
+	Status   data.Map    `json:"status,omitempty"`
+	Meta     interface{} `json:"meta,omitempty"`
+}
+
+// NewSink returns the result of the sink node. It generates status and
+// meta information if detailed argument is true.
+func NewSink(sn core.SinkNode, detailed bool) *Sink {
+	s := &Sink{
+		NodeType: core.NTSink.String(),
+		Name:     sn.Name(),
+		State:    sn.State().Get().String(),
+	}
+
+	if detailed {
+		s.Status = sn.Status()
+		s.Meta = sn.Meta()
+	}
+	return s
+}
