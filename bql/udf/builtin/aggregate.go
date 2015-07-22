8259bf33590e2f47c1c8ae5396d7608ad4cac2fa
Tobias Pfeiffer <tgp@preferred.jp>
2015-07-22 10:48:24 +0900

    reshape count() to look like the other UDFs

--- bql/udf/builtin/aggregate.go
+++ bql/udf/builtin/aggregate.go
@@ -1,39 +1,55 @@
 package builtin
 
 import (
+	"bytes"
 	"fmt"
+	"math"
 	"pfi/sensorbee/sensorbee/bql/udf"
 	"pfi/sensorbee/sensorbee/core"
 	"pfi/sensorbee/sensorbee/data"
 )
 
-type countFuncTmpl struct {
+// singleParamAggFunc is a template for aggregate functions that
+// have exactly one parameter
+type singleParamAggFunc struct {
+	aggFun func([]data.Value) (data.Value, error)
 }
 
-func (f *countFuncTmpl) Call(ctx *core.Context, args ...data.Value) (data.Value, error) {
-	if len(args) != 1 {
-		return nil, fmt.Errorf("count takes exactly one argument")
-	}
-	arr, err := data.AsArray(args[0])
-	if err != nil {
-		return nil, fmt.Errorf("count needs an array input, not %v", args[0])
-	}
-	// count() is O(n) in the spirit of PostgreSQL
-	c := int64(0)
-	for _, item := range arr {
-		if item.Type() != data.TypeNull {
-			c++
-		}
-	}
-	return data.Int(c), nil
-}
-
-func (f *countFuncTmpl) Accept(arity int) bool {
+func (f *singleParamAggFunc) Accept(arity int) bool {
 	return arity == 1
 }
 
-func (f *countFuncTmpl) IsAggregationParameter(k int) bool {
+func (f *singleParamAggFunc) IsAggregationParameter(k int) bool {
 	return k == 0
 }
 
-var countFunc udf.UDF = &countFuncTmpl{}
+func (f *singleParamAggFunc) Call(ctx *core.Context, args ...data.Value) (data.Value, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("function takes exactly one argument")
+	}
+	arr, err := data.AsArray(args[0])
+	if err != nil {
+		return nil, fmt.Errorf("function needs array input, not %T", args[0])
+	}
+	return f.aggFun(arr)
+}
+
+// countFunc is an aggregate function that counts the number
+// of non-null values passed in.
+//
+// It can be used in BQL as `count`.
+//
+//  Input: anything (aggregated)
+//  Return Type: Int
+var countFunc udf.UDF = &singleParamAggFunc{
+	aggFun: func(arr []data.Value) (data.Value, error) {
+		// count() is O(n) in the spirit of PostgreSQL
+		c := int64(0)
+		for _, item := range arr {
+			if item.Type() != data.TypeNull {
+				c++
+			}
+		}
+		return data.Int(c), nil
+	},
+}
