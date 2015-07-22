efcb70384713d5ddc52f8eab09e9dc35d15d9bdf
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-07-22 16:48:48 -0700

    Lower cased string representation of EventType

--- core/default_topology_tracing_test.go
+++ core/default_topology_tracing_test.go
@@ -160,20 +160,20 @@ func TestDefaultTopologyTupleTracing(t *testing.T) {
 			Convey("Then tracer has 2 kind of route from source1", func() {
 				// make expected routes
 				route1 := []string{
-					"OUTPUT so1", "INPUT box1", "OUTPUT box1", "INPUT box2",
-					"OUTPUT box2", "INPUT box4", "OUTPUT box4", "INPUT si1",
+					"output so1", "input box1", "output box1", "input box2",
+					"output box2", "input box4", "output box4", "input si1",
 				}
 				route2 := []string{
-					"OUTPUT so1", "INPUT box1", "OUTPUT box1", "INPUT box3",
-					"OUTPUT box3", "INPUT box4", "OUTPUT box4", "INPUT si1",
+					"output so1", "input box1", "output box1", "input box3",
+					"output box3", "input box4", "output box4", "input si1",
 				}
 				route3 := []string{
-					"OUTPUT so2", "INPUT box1", "OUTPUT box1", "INPUT box2",
-					"OUTPUT box2", "INPUT box4", "OUTPUT box4", "INPUT si1",
+					"output so2", "input box1", "output box1", "input box2",
+					"output box2", "input box4", "output box4", "input si1",
 				}
 				route4 := []string{
-					"OUTPUT so2", "INPUT box1", "OUTPUT box1", "INPUT box3",
-					"OUTPUT box3", "INPUT box4", "OUTPUT box4", "INPUT si1",
+					"output so2", "input box1", "output box1", "input box3",
+					"output box3", "input box4", "output box4", "input si1",
 				}
 				eRoutes := []string{
 					strings.Join(route1, "->"),
@@ -199,20 +199,20 @@ func TestDefaultTopologyTupleTracing(t *testing.T) {
 			Convey("Then tracer has 2 kind of route from source2", func() {
 				// make expected routes
 				route1 := []string{
-					"OUTPUT so1", "INPUT box1", "OUTPUT box1", "INPUT box2",
-					"OUTPUT box2", "INPUT box4", "OUTPUT box4", "INPUT si2",
+					"output so1", "input box1", "output box1", "input box2",
+					"output box2", "input box4", "output box4", "input si2",
 				}
 				route2 := []string{
-					"OUTPUT so1", "INPUT box1", "OUTPUT box1", "INPUT box3",
-					"OUTPUT box3", "INPUT box4", "OUTPUT box4", "INPUT si2",
+					"output so1", "input box1", "output box1", "input box3",
+					"output box3", "input box4", "output box4", "input si2",
 				}
 				route3 := []string{
-					"OUTPUT so2", "INPUT box1", "OUTPUT box1", "INPUT box2",
-					"OUTPUT box2", "INPUT box4", "OUTPUT box4", "INPUT si2",
+					"output so2", "input box1", "output box1", "input box2",
+					"output box2", "input box4", "output box4", "input si2",
 				}
 				route4 := []string{
-					"OUTPUT so2", "INPUT box1", "OUTPUT box1", "INPUT box3",
-					"OUTPUT box3", "INPUT box4", "OUTPUT box4", "INPUT si2",
+					"output so2", "input box1", "output box1", "input box3",
+					"output box3", "input box4", "output box4", "input si2",
 				}
 				eRoutes := []string{
 					strings.Join(route1, "->"),
