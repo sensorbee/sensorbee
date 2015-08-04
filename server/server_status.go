7f3ab5f8e2916f8cefaa6bc29e510373b8361840
Shuzo Kashihara <kashihara@preferred.jp>
2015-08-04 14:40:50 +0900

    Add warn log to runtime_status

--- server/server_status.go
+++ server/server_status.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/user"
 	"runtime"
+	"sync"
 )
 
 type serverStatus struct {
@@ -26,13 +27,27 @@ func (ss *serverStatus) RuntimeStatus(rw web.ResponseWriter, req *web.Request) {
 		"goversion":     runtime.Version(),
 		"pid":           os.Getpid(),
 	}
-	if dir, err := os.Getwd(); err == nil {
+
+	var once sync.Once
+	logOnce := func(name string) {
+		once.Do(func() {
+			ss.APIContext.Log().Warnf("runtime status '%v' isn't supported on this environment (this log is only written once)", name)
+		})
+	}
+
+	if dir, err := os.Getwd(); err != nil {
+		logOnce("working_directory")
+	} else {
 		res["working_directory"] = dir
 	}
-	if host, err := os.Hostname(); err == nil {
+	if host, err := os.Hostname(); err != nil {
+		logOnce("hostname")
+	} else {
 		res["hostname"] = host
 	}
-	if user, err := user.Current(); err == nil {
+	if user, err := user.Current(); err != nil {
+		logOnce("user")
+	} else {
 		res["user"] = user.Username
 	}
 	ss.RenderJSON(res)
