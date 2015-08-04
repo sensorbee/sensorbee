29cae8cf3da05453b2d9619df288c0dcbf36296f
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2015-08-03 17:05:09 -0700

    Reduce number of appends

--- data/hash.go
+++ data/hash.go
@@ -30,8 +30,8 @@ func Equal(v1 Value, v2 Value) bool {
 	return false
 }
 
-func appendInt32(b []byte, i int32) []byte {
-	return append(b,
+func appendInt32(b []byte, t TypeID, i int32) []byte {
+	return append(b, byte(t),
 		byte(i&0xff),
 		byte((i>>8)&0xff),
 		byte((i>>16)&0xff),
@@ -39,8 +39,8 @@ func appendInt32(b []byte, i int32) []byte {
 	)
 }
 
-func appendInt64(b []byte, i int64) []byte {
-	return append(b,
+func appendInt64(b []byte, t TypeID, i int64) []byte {
+	return append(b, byte(t),
 		byte(i&0xff),
 		byte((i>>8)&0xff),
 		byte((i>>16)&0xff),
@@ -56,8 +56,7 @@ func updateHash(v Value, h io.Writer, buffer []byte) []byte {
 	switch v.Type() {
 	case TypeBlob:
 		b, _ := v.asBlob()
-		buffer = append(buffer, byte(TypeBlob))
-		buffer = appendInt32(buffer, int32(len(b)))
+		buffer = appendInt32(buffer, TypeBlob, int32(len(b)))
 		h.Write(buffer)
 		h.Write(b)
 	case TypeBool:
@@ -73,41 +72,36 @@ func updateHash(v Value, h io.Writer, buffer []byte) []byte {
 		if float64(int64(f)) == f {
 			return updateHash(Int(f), h, buffer)
 		}
-		buffer = append(buffer, byte(TypeFloat))
-		buffer = appendInt64(buffer, int64(math.Float64bits(f)))
+		buffer = appendInt64(buffer, TypeFloat, int64(math.Float64bits(f)))
 		h.Write(buffer)
 	case TypeInt:
 		i, _ := v.asInt()
-		buffer = append(buffer, byte(TypeInt))
-		buffer = appendInt64(buffer, i)
+		buffer = appendInt64(buffer, TypeInt, i)
 		h.Write(buffer)
 	case TypeNull:
 		buffer = append(buffer, byte(TypeNull))
 		h.Write(buffer)
 	case TypeString:
 		s, _ := v.asString()
-		buffer = append(buffer, byte(TypeString))
-		buffer = appendInt32(buffer, int32(len(s)))
+		buffer = appendInt32(buffer, TypeString, int32(len(s)))
 		h.Write(buffer)
 		io.WriteString(h, s)
 	case TypeTimestamp:
 		t, _ := v.asTimestamp()
-		buffer = append(buffer, byte(TypeTimestamp))
-		buffer = appendInt64(buffer, t.Unix())
-		buffer = appendInt32(buffer, int32(t.Nanosecond()/1000)) // Use microseconds
+		buffer = appendInt64(buffer, TypeTimestamp, t.Unix())
+		// TODO: This TypeInt isn't necessary.
+		buffer = appendInt32(buffer, TypeInt, int32(t.Nanosecond()/1000)) // Use microseconds
 		h.Write(buffer)
 	case TypeArray:
 		a, _ := v.asArray()
-		buffer = append(buffer, byte(TypeArray))
-		buffer = appendInt32(buffer, int32(len(a)))
+		buffer = appendInt32(buffer, TypeArray, int32(len(a)))
 		h.Write(buffer)
 		for _, item := range a {
 			buffer = updateHash(item, h, buffer[:0])
 		}
 	case TypeMap:
 		m, _ := v.asMap()
-		buffer = append(buffer, byte(TypeMap))
-		buffer = appendInt32(buffer, int32(len(m)))
+		buffer = appendInt32(buffer, TypeMap, int32(len(m)))
 		h.Write(buffer)
 
 		// TODO: reduce this allocation
