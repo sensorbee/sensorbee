d535773270445cbac415b147f03c9a9ff51f9dbd
Gentaro Watanabe <g.wtnb@preferred.jp>
2015-11-24 16:16:48 +0900

    Modify sharedStateSink to refer extract sinks by name in Write().

--- core/shared_state.go
+++ core/shared_state.go
@@ -244,7 +244,7 @@ func (r *defaultSharedStateRegistry) Remove(name string) (SharedState, error) {
 // writer points a shared state. sharedStateSink will point to the same shared state
 // even after the state is removed from the context.
 type sharedStateSink struct {
-	writer Writer
+	name string
 }
 
 // NewSharedStateSink creates a sink that writes to SharedState.
@@ -256,19 +256,30 @@ func NewSharedStateSink(ctx *Context, name string) (Sink, error) {
 	}
 
 	// It fails if the shared state cannot be written
-	writer, ok := state.(Writer)
+	_, ok := state.(Writer)
 	if !ok {
 		return nil, fmt.Errorf("'%v' state cannot be written", name)
 	}
 
 	s := &sharedStateSink{
-		writer: writer,
+		name: name,
 	}
 	return s, nil
 }
 
 func (s *sharedStateSink) Write(ctx *Context, t *Tuple) error {
-	return s.writer.Write(ctx, t)
+	state, err := ctx.SharedStates.Get(s.name)
+	if err != nil {
+		return err
+	}
+
+	// It fails if the shared state cannot be written
+	writer, ok := state.(Writer)
+	if !ok {
+		return fmt.Errorf("'%v' state cannot be written", s.name)
+	}
+
+	return writer.Write(ctx, t)
 }
 
 func (s *sharedStateSink) Close(ctx *Context) error {
