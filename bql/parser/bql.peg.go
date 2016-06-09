ee6c6a226cca456b18d5a6fff3e4dd0406590966
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2016-06-09 16:01:12 +0900

    Apply latest peg command

--- bql/parser/bql.peg.go
+++ bql/parser/bql.peg.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-const end_symbol rune = 1114112
+const endSymbol rune = 1114112
 
 /* The rule types inferred from the grammar are below. */
 type pegRule uint16
@@ -336,9 +336,9 @@ const (
 	ruleAction132
 	ruleAction133
 
-	rulePre_
-	rule_In_
-	rule_Suf
+	rulePre
+	ruleIn
+	ruleSuf
 )
 
 var rul3s = [...]string{
@@ -700,8 +700,8 @@ func (node *node32) print(depth int, buffer string) {
 	}
 }
 
-func (ast *node32) Print(buffer string) {
-	ast.print(0, buffer)
+func (node *node32) Print(buffer string) {
+	node.print(0, buffer)
 }
 
 type element struct {
@@ -809,7 +809,7 @@ func (t *tokens32) PreOrder() (<-chan state32, [][]token32) {
 	s, ordered := make(chan state32, 6), t.Order()
 	go func() {
 		var states [8]state32
-		for i, _ := range states {
+		for i := range states {
 			states[i].depths = make([]int32, len(ordered))
 		}
 		depths, state, depth := make([]int32, len(ordered)), 0, 1
@@ -831,14 +831,14 @@ func (t *tokens32) PreOrder() (<-chan state32, [][]token32) {
 					if c, j := ordered[depth][i-1], depths[depth-1]; a.isParentOf(c) &&
 						(j < 2 || !ordered[depth-1][j-2].isParentOf(c)) {
 						if c.end != b.begin {
-							write(token32{pegRule: rule_In_, begin: c.end, end: b.begin}, true)
+							write(token32{pegRule: ruleIn, begin: c.end, end: b.begin}, true)
 						}
 						break
 					}
 				}
 
 				if a.begin < b.begin {
-					write(token32{pegRule: rulePre_, begin: a.begin, end: b.begin}, true)
+					write(token32{pegRule: rulePre, begin: a.begin, end: b.begin}, true)
 				}
 				break
 			}
@@ -859,7 +859,7 @@ func (t *tokens32) PreOrder() (<-chan state32, [][]token32) {
 					b = c
 					continue depthFirstSearch
 				} else if parent && b.end != a.end {
-					write(token32{pegRule: rule_Suf, begin: b.end, end: a.end}, true)
+					write(token32{pegRule: ruleSuf, begin: b.end, end: a.end}, true)
 				}
 
 				depth--
@@ -948,7 +948,7 @@ func (t *tokens32) Error() []token32 {
 	ordered := t.Order()
 	length := len(ordered)
 	tokens, length := make([]token32, length), length-1
-	for i, _ := range tokens {
+	for i := range tokens {
 		o := ordered[length-i]
 		if len(o) > 1 {
 			tokens[i] = o[len(o)-2].getToken32()
@@ -987,6 +987,7 @@ type bqlPegBackend struct {
 	rules  [322]func() bool
 	Parse  func(rule ...int) error
 	Reset  func()
+	Pretty bool
 	tokenTree
 }
 
@@ -996,12 +997,12 @@ type textPosition struct {
 
 type textPositionMap map[int]textPosition
 
-func translatePositions(buffer string, positions []int) textPositionMap {
+func translatePositions(buffer []rune, positions []int) textPositionMap {
 	length, translations, j, line, symbol := len(positions), make(textPositionMap, len(positions)), 0, 1, 0
 	sort.Ints(positions)
 
 search:
-	for i, c := range []rune(buffer) {
+	for i, c := range buffer {
 		if c == '\n' {
 			line, symbol = line+1, 0
 		} else {
@@ -1022,24 +1023,29 @@ search:
 }
 
 type parseError struct {
-	p *bqlPegBackend
+	p   *bqlPegBackend
+	max token32
 }
 
 func (e *parseError) Error() string {
-	tokens, error := e.p.tokenTree.Error(), "\n"
+	tokens, error := []token32{e.max}, "\n"
 	positions, p := make([]int, 2*len(tokens)), 0
 	for _, token := range tokens {
 		positions[p], p = int(token.begin), p+1
 		positions[p], p = int(token.end), p+1
 	}
-	translations := translatePositions(e.p.Buffer, positions)
+	translations := translatePositions(e.p.buffer, positions)
+	format := "parse error near %v (line %v symbol %v - line %v symbol %v):\n%v\n"
+	if e.p.Pretty {
+		format = "parse error near \x1B[34m%v\x1B[m (line %v symbol %v - line %v symbol %v):\n%v\n"
+	}
 	for _, token := range tokens {
 		begin, end := int(token.begin), int(token.end)
-		error += fmt.Sprintf("parse error near \x1B[34m%v\x1B[m (line %v symbol %v - line %v symbol %v):\n%v\n",
+		error += fmt.Sprintf(format,
 			rul3s[token.pegRule],
 			translations[begin].line, translations[begin].symbol,
 			translations[end].line, translations[end].symbol,
-			/*strconv.Quote(*/ e.p.Buffer[begin:end] /*)*/)
+			strconv.Quote(string(e.p.buffer[begin:end])))
 	}
 
 	return error
@@ -1630,11 +1636,12 @@ func (p *bqlPegBackend) Execute() {
 
 func (p *bqlPegBackend) Init() {
 	p.buffer = []rune(p.Buffer)
-	if len(p.buffer) == 0 || p.buffer[len(p.buffer)-1] != end_symbol {
-		p.buffer = append(p.buffer, end_symbol)
+	if len(p.buffer) == 0 || p.buffer[len(p.buffer)-1] != endSymbol {
+		p.buffer = append(p.buffer, endSymbol)
 	}
 
 	var tree tokenTree = &tokens32{tree: make([]token32, math.MaxInt16)}
+	var max token32
 	position, depth, tokenIndex, buffer, _rules := uint32(0), uint32(0), 0, p.buffer, p.rules
 
 	p.Parse = func(rule ...int) error {
@@ -1648,7 +1655,7 @@ func (p *bqlPegBackend) Init() {
 			p.tokenTree.trim(tokenIndex)
 			return nil
 		}
-		return &parseError{p}
+		return &parseError{p, max}
 	}
 
 	p.Reset = func() {
@@ -1661,10 +1668,13 @@ func (p *bqlPegBackend) Init() {
 		}
 		tree.Add(rule, begin, position, depth, tokenIndex)
 		tokenIndex++
+		if begin != position && position > max.end {
+			max = token32{rule, begin, position, depth}
+		}
 	}
 
 	matchDot := func() bool {
-		if buffer[position] != end_symbol {
+		if buffer[position] != endSymbol {
 			position++
 			return true
 		}
