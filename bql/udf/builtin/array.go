7feed2cc91ae730d815941d9b77bd6fc209d735f
Tobias Pfeiffer <tgp@preferred.jp>
2016-07-14 09:55:10 +0900

    implement array_length using UnaryFunc

--- bql/udf/builtin/array.go
+++ bql/udf/builtin/array.go
@@ -7,29 +7,6 @@ import (
 	"gopkg.in/sensorbee/sensorbee.v0/data"
 )
 
-type arrayLengthFuncTmpl struct {
-	singleParamFunc
-}
-
-func (f *arrayLengthFuncTmpl) Call(ctx *core.Context, args ...data.Value) (val data.Value, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("%v", r)
-		}
-	}()
-	if len(args) != 1 {
-		return nil, fmt.Errorf("function takes exactly one argument")
-	}
-	arg := args[0]
-	if arg.Type() == data.TypeNull {
-		return data.Null{}, nil
-	} else if arg.Type() == data.TypeArray {
-		a, _ := data.AsArray(arg)
-		return data.Int(len(a)), nil
-	}
-	return nil, fmt.Errorf("%v is not an array", arg)
-}
-
 // arrayLengthFunc returns the length of the given array.
 // NULL elements are counted as well.
 //
@@ -37,4 +14,12 @@ func (f *arrayLengthFuncTmpl) Call(ctx *core.Context, args ...data.Value) (val d
 //
 //  Input: Array
 //  Return Type: Int
-var arrayLengthFunc udf.UDF = &arrayLengthFuncTmpl{}
+var arrayLengthFunc udf.UDF = udf.UnaryFunc(func(ctx *core.Context, arg data.Value) (val data.Value, err error) {
+	if arg.Type() == data.TypeNull {
+		return data.Null{}, nil
+	} else if arg.Type() == data.TypeArray {
+		a, _ := data.AsArray(arg)
+		return data.Int(len(a)), nil
+	}
+	return nil, fmt.Errorf("%v is not an array", arg)
+})
