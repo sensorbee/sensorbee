ea7f450118639086ec21b60468dcc679a482d557
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2016-12-05 15:44:32 -0800

    Version 0.6.0

--- version/version.go
+++ version/version.go
@@ -2,14 +2,14 @@ package version
 
 const (
 	// Version is the current version of SensorBee.
-	Version = "0.5.2"
+	Version = "0.6.0"
 
 	// Major is the major version of the current SensorBee.
 	Major = 0
 
 	// Minor is the minor version of the current SensorBee.
-	Minor = 5
+	Minor = 6
 
 	// Revision is the revision number of the current SensorBee version.
-	Revision = 2
+	Revision = 0
 )
