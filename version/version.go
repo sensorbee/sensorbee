3a30527faf5e0f8f94a21aef45430ab21964f7aa
disktnk <duaipp@gmail.com>
2017-07-21 11:12:00 +0900

    Version 0.7.0

--- version/version.go
+++ version/version.go
@@ -2,14 +2,14 @@ package version
 
 const (
 	// Version is the current version of SensorBee.
-	Version = "0.6.1"
+	Version = "0.7.0"
 
 	// Major is the major version of the current SensorBee.
 	Major = 0
 
 	// Minor is the minor version of the current SensorBee.
-	Minor = 6
+	Minor = 7
 
 	// Revision is the revision number of the current SensorBee version.
-	Revision = 1
+	Revision = 0
 )
